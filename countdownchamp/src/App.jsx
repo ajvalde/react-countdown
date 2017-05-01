4b4ff4117c40a0b6dd86f6387b6180ee0f7bfc70
Anthony <anthonyguitarman13@gmail.com>
2017-05-01 13:40:09 -0600

    layout done

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
     render(){
         return(
-        <div>Countdown Champ, App Component</div>
+        <div className='App'>
+            <div className='app-title'>Countdown to December 25, 2017</div>
+            <div>
+                <div className='clock-days'>14 Days</div>
+                <div className='clock-hours'>30 Hours</div>
+                <div className='clock-minutes'>15 Minutes</div>
+                <div className='clock-seconds'>20 Seconds</div>
+            </div>
+            <div>
+            <input placeholder="new date"/>
+            <button>Submit</button>
+            </div>
+        </div>
 
         )
     }
--- countdownchamp/src/index.js
+++ countdownchamp/src/index.js
@@ -3,5 +3,5 @@ import ReactDom from 'react-dom';
 import App from './App.jsx'
 
 ReactDom.render(
-    <App></App>, document.getElementById('root')
+    <App />, document.getElementById('root')
 );
